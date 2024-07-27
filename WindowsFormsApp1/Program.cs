fde257ee15944ff2daac199d4585b059c0b5583a
CarlosMartin2005 <cpenap@unah.hn>
2024-07-26 19:27:07 -0600

    Aprendiendo el funcionamiento de hilos en C#

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,10 +14,38 @@ namespace WindowsFormsApp1
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        {   
+            // Crear dos hilos
+            Thread hilo1 = new Thread(new ThreadStart(ejec1));
+            Thread hilo2 = new Thread(new ThreadStart(ejec2));
+
+            // Iniciar los hilos
+            hilo1.Start();
+            hilo2.Start();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+        }
+
+        static void ejec1()
+        {
+            // Código que ejecutará el hilo 1
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Hilo 1 ejecutándose...");
+                Thread.Sleep(1000); // Pausar el hilo por 1 segundo
+            }
+        }
+
+        static void ejec2()
+        {
+            // Código que ejecutará el hilo 2
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Hilo 2 ejecutándose...");
+                Thread.Sleep(1000); // Pausar el hilo por 1 segundo
+            }
         }
     }
 }
